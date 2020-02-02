239af9180b3160e7b004e22b58b4a7f9a5f1c544
NKaty <nd.katy@gmail.com>
2020-02-02 21:01:18 +0300

    initial commit

---
+++ application.py
@@ -0,0 +1,13 @@
+import os
+
+from flask import Flask
+from flask_socketio import SocketIO, emit
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+socketio = SocketIO(app)
+
+
+@app.route("/")
+def index():
+    return "Project 2: TODO"
