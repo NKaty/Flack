cba3cb64da6a56e279fd677326b9fd01ed658efc
NKaty <nd.katy@gmail.com>
2020-02-03 18:27:12 +0300

    add flask-bootstrap

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
+from flask_bootstrap import Bootstrap
 from flask_session import Session
 from flask_socketio import SocketIO
 from flask_sqlalchemy import SQLAlchemy
 
 from config import config
 
+bootstrap = Bootstrap()
 db = SQLAlchemy()
 socketio = SocketIO()
 session = Session()
@@ -14,6 +16,7 @@ def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config[config_name])
 
+    bootstrap.init_app(app)
     db.init_app(app)
     session.init_app(app)
     socketio.init_app(app, manage_session=False)
