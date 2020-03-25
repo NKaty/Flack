4adb089fcfde4a5c986d75521612e590b20d55b3
NKaty <nd.katy@gmail.com>
2020-03-25 21:34:54 +0300

    fix app settings

--- app/__init__.py
+++ app/__init__.py
@@ -8,11 +8,11 @@ from sqlalchemy import MetaData
 from config import config
 
 naming_convention = {
-    "ix": 'ix_%(column_0_label)s',
-    "uq": "uq_%(table_name)s_%(column_0_name)s",
-    "ck": "ck_%(table_name)s_%(column_0_name)s",
-    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-    "pk": "pk_%(table_name)s"
+    'ix': 'ix_%(column_0_label)s',
+    'uq': 'uq_%(table_name)s_%(column_0_name)s',
+    'ck': 'ck_%(table_name)s_%(column_0_name)s',
+    'fk': 'fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s',
+    'pk': 'pk_%(table_name)s'
 }
 
 db = SQLAlchemy(metadata=MetaData(naming_convention=naming_convention))
@@ -21,7 +21,7 @@ session = Session()
 
 login_manager = LoginManager()
 login_manager.login_view = 'auth.login'
-login_manager.login_message_category = "danger"
+login_manager.login_message_category = 'danger'
 
 
 def create_app(config_name):
--- chat.py
+++ chat.py
@@ -4,19 +4,19 @@ from dotenv import load_dotenv
 from flask_migrate import Migrate
 
 from app import create_app, db, socketio
-from app.models import User, Channel, Message
+from app.models import User, Channel, Message, File
 
 dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
 if os.path.exists(dotenv_path):
     load_dotenv(dotenv_path)
 
-app = create_app(os.getenv('FLASK_flask CONFIG') or 'default')
+app = create_app(os.getenv('CONFIG') or 'default')
 migrate = Migrate(app, db, render_as_batch=True)
 
 
 @app.shell_context_processor
 def make_shell_context():
-    return dict(db=db, User=User, Channel=Channel, Message=Message)
+    return dict(db=db, User=User, Channel=Channel, Message=Message, File=File)
 
 
 if __name__ == '__main__':
